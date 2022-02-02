30fa40287ed55315d671b8296ea22ea8db34949e
Aigo <87446059+aigOffline@users.noreply.github.com>
2022-02-02 16:32:26 -0500

    Update Question.sql

--- Question.sql
+++ Question.sql
@@ -11,8 +11,8 @@ Sort the result set by last name in ascending sequence.
 Return only the customers whose last name begins with letters from M to Z.
 
 ------------------------------------------------------------------------*/
-SELECT first_name, last_name, CONCAT(last_name, ", ", first_name) AS full_name
-FROM customers
-WHERE last_name >= "M"
-ORDER BY last_name ASC;
+SELECT  CONCAT(last_name, ', ', first_name) AS full_name
+FROM Customers 
+where last_name REGEXP’^[M-Z]
+ORDER BY last_name ACS;
 
