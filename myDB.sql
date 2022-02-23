abdcbae74cf0e60498e8e8ff099e34be426a8150
Aigo <87446059+aigOffline@users.noreply.github.com>
2022-02-23 16:38:27 -0500

    Update myDB.sql

--- myDB.sql
+++ myDB.sql
@@ -1,6 +1,561 @@
 /********************************************************
 * This script creates the database named my_shop 
 *********************************************************/
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.1
+-- Dumped by pg_dump version 14.1
+
+-- Started on 2022-02-23 16:36:10 EST
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 213 (class 1259 OID 32892)
+-- Name: addresses; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.addresses (
+    line1 character varying(60) NOT NULL,
+    line2 character varying(60) DEFAULT NULL::character varying,
+    city character varying(40) NOT NULL,
+    state character varying(2) NOT NULL,
+    country character varying(40) DEFAULT 'USA'::character varying,
+    zip_code character varying(10) NOT NULL,
+    phone character varying(12) NOT NULL,
+    id bigint NOT NULL
+);
+
+
+ALTER TABLE public.addresses OWNER TO postgres;
+
+--
+-- TOC entry 210 (class 1259 OID 32857)
+-- Name: categories; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.categories (
+    category_id bigint NOT NULL,
+    category_name character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.categories OWNER TO postgres;
+
+--
+-- TOC entry 209 (class 1259 OID 32856)
+-- Name: categories_category_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.categories_category_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.categories_category_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 3648 (class 0 OID 0)
+-- Dependencies: 209
+-- Name: categories_category_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.categories_category_id_seq OWNED BY public.categories.category_id;
+
+
+--
+-- TOC entry 217 (class 1259 OID 32918)
+-- Name: order_items; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.order_items (
+    id bigint NOT NULL,
+    order_id integer NOT NULL,
+    product_id integer NOT NULL,
+    item_price numeric(10,2) NOT NULL,
+    discount_amount numeric(10,2) NOT NULL,
+    quantity integer NOT NULL
+);
+
+
+ALTER TABLE public.order_items OWNER TO postgres;
+
+--
+-- TOC entry 216 (class 1259 OID 32917)
+-- Name: order_items_item_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.order_items_item_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.order_items_item_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 3649 (class 0 OID 0)
+-- Dependencies: 216
+-- Name: order_items_item_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.order_items_item_id_seq OWNED BY public.order_items.id;
+
+
+--
+-- TOC entry 215 (class 1259 OID 32906)
+-- Name: orders; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.orders (
+    id bigint NOT NULL,
+    user_id integer NOT NULL,
+    order_date date NOT NULL,
+    ship_amount numeric(10,2) NOT NULL,
+    tax_amount numeric(10,2) NOT NULL,
+    ship_date date,
+    ship_address_id integer NOT NULL,
+    card_type character varying(50) NOT NULL,
+    card_number character(16) NOT NULL,
+    card_expires character(7) NOT NULL,
+    billing_address_id integer NOT NULL
+);
+
+
+ALTER TABLE public.orders OWNER TO postgres;
+
+--
+-- TOC entry 214 (class 1259 OID 32905)
+-- Name: orders_order_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.orders_order_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.orders_order_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 3650 (class 0 OID 0)
+-- Dependencies: 214
+-- Name: orders_order_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.orders_order_id_seq OWNED BY public.orders.id;
+
+
+--
+-- TOC entry 220 (class 1259 OID 32962)
+-- Name: person; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.person (
+    id bigint NOT NULL,
+    first_name character varying(255),
+    last_name character varying(255),
+    middle_name character varying(255),
+    birth_date date,
+    address_id bigint
+);
+
+
+ALTER TABLE public.person OWNER TO postgres;
+
+--
+-- TOC entry 212 (class 1259 OID 32866)
+-- Name: products; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.products (
+    id bigint NOT NULL,
+    category_id integer NOT NULL,
+    description character varying(500) NOT NULL,
+    price numeric(10,2) NOT NULL,
+    discount numeric(10,2) DEFAULT 0.00 NOT NULL,
+    date_created date
+);
+
+
+ALTER TABLE public.products OWNER TO postgres;
+
+--
+-- TOC entry 211 (class 1259 OID 32865)
+-- Name: products_product_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.products_product_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.products_product_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 3651 (class 0 OID 0)
+-- Dependencies: 211
+-- Name: products_product_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.products_product_id_seq OWNED BY public.products.id;
+
+
+--
+-- TOC entry 219 (class 1259 OID 32952)
+-- Name: roles; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.roles (
+    "id " bigint NOT NULL,
+    role_name character varying(255)
+);
+
+
+ALTER TABLE public.roles OWNER TO postgres;
+
+--
+-- TOC entry 218 (class 1259 OID 32944)
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    email_address character varying(255) NOT NULL,
+    password character varying(255) NOT NULL,
+    role_id bigint,
+    person_id bigint
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- TOC entry 3461 (class 2604 OID 32860)
+-- Name: categories category_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.categories ALTER COLUMN category_id SET DEFAULT nextval('public.categories_category_id_seq'::regclass);
+
+
+--
+-- TOC entry 3467 (class 2604 OID 32921)
+-- Name: order_items id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.order_items ALTER COLUMN id SET DEFAULT nextval('public.order_items_item_id_seq'::regclass);
+
+
+--
+-- TOC entry 3466 (class 2604 OID 32909)
+-- Name: orders id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders ALTER COLUMN id SET DEFAULT nextval('public.orders_order_id_seq'::regclass);
+
+
+--
+-- TOC entry 3462 (class 2604 OID 32869)
+-- Name: products id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.products ALTER COLUMN id SET DEFAULT nextval('public.products_product_id_seq'::regclass);
+
+
+--
+-- TOC entry 3635 (class 0 OID 32892)
+-- Dependencies: 213
+-- Data for Name: addresses; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.addresses (line1, line2, city, state, country, zip_code, phone, id) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3632 (class 0 OID 32857)
+-- Dependencies: 210
+-- Data for Name: categories; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.categories (category_id, category_name) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3639 (class 0 OID 32918)
+-- Dependencies: 217
+-- Data for Name: order_items; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.order_items (id, order_id, product_id, item_price, discount_amount, quantity) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3637 (class 0 OID 32906)
+-- Dependencies: 215
+-- Data for Name: orders; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.orders (id, user_id, order_date, ship_amount, tax_amount, ship_date, ship_address_id, card_type, card_number, card_expires, billing_address_id) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3642 (class 0 OID 32962)
+-- Dependencies: 220
+-- Data for Name: person; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.person (id, first_name, last_name, middle_name, birth_date, address_id) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3634 (class 0 OID 32866)
+-- Dependencies: 212
+-- Data for Name: products; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.products (id, category_id, description, price, discount, date_created) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3641 (class 0 OID 32952)
+-- Dependencies: 219
+-- Data for Name: roles; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.roles ("id ", role_name) FROM stdin;
+1	admin
+2	customer
+\.
+
+
+--
+-- TOC entry 3640 (class 0 OID 32944)
+-- Dependencies: 218
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (email_address, password, role_id, person_id) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3652 (class 0 OID 0)
+-- Dependencies: 209
+-- Name: categories_category_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.categories_category_id_seq', 1, false);
+
+
+--
+-- TOC entry 3653 (class 0 OID 0)
+-- Dependencies: 216
+-- Name: order_items_item_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.order_items_item_id_seq', 1, false);
+
+
+--
+-- TOC entry 3654 (class 0 OID 0)
+-- Dependencies: 214
+-- Name: orders_order_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.orders_order_id_seq', 1, false);
+
+
+--
+-- TOC entry 3655 (class 0 OID 0)
+-- Dependencies: 211
+-- Name: products_product_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.products_product_id_seq', 1, false);
+
+
+--
+-- TOC entry 3475 (class 2606 OID 32976)
+-- Name: addresses addresses_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.addresses
+    ADD CONSTRAINT addresses_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3469 (class 2606 OID 32864)
+-- Name: categories categories_category_name_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.categories
+    ADD CONSTRAINT categories_category_name_key UNIQUE (category_name);
+
+
+--
+-- TOC entry 3471 (class 2606 OID 32862)
+-- Name: categories categories_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.categories
+    ADD CONSTRAINT categories_pkey PRIMARY KEY (category_id);
+
+
+--
+-- TOC entry 3479 (class 2606 OID 32923)
+-- Name: order_items order_items_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.order_items
+    ADD CONSTRAINT order_items_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3477 (class 2606 OID 32911)
+-- Name: orders orders_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT orders_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3483 (class 2606 OID 32968)
+-- Name: person person_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.person
+    ADD CONSTRAINT person_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3473 (class 2606 OID 32874)
+-- Name: products products_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.products
+    ADD CONSTRAINT products_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3481 (class 2606 OID 32956)
+-- Name: roles roles_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.roles
+    ADD CONSTRAINT roles_pkey PRIMARY KEY ("id ");
+
+
+--
+-- TOC entry 3491 (class 2606 OID 32977)
+-- Name: person address_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.person
+    ADD CONSTRAINT address_fkey FOREIGN KEY (address_id) REFERENCES public.addresses(id) NOT VALID;
+
+
+--
+-- TOC entry 3485 (class 2606 OID 32988)
+-- Name: orders billing_address_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT billing_address_fkey FOREIGN KEY (billing_address_id) REFERENCES public.addresses(id) NOT VALID;
+
+
+--
+-- TOC entry 3487 (class 2606 OID 32924)
+-- Name: order_items items_fk_orders; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.order_items
+    ADD CONSTRAINT items_fk_orders FOREIGN KEY (order_id) REFERENCES public.orders(id);
+
+
+--
+-- TOC entry 3488 (class 2606 OID 32929)
+-- Name: order_items items_fk_products; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.order_items
+    ADD CONSTRAINT items_fk_products FOREIGN KEY (product_id) REFERENCES public.products(id);
+
+
+--
+-- TOC entry 3490 (class 2606 OID 32970)
+-- Name: users person_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT person_fkey FOREIGN KEY (person_id) REFERENCES public.person(id) NOT VALID;
+
+
+--
+-- TOC entry 3484 (class 2606 OID 32875)
+-- Name: products products_fk_categories; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.products
+    ADD CONSTRAINT products_fk_categories FOREIGN KEY (category_id) REFERENCES public.categories(category_id);
+
+
+--
+-- TOC entry 3489 (class 2606 OID 32957)
+-- Name: users role_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT role_fkey FOREIGN KEY (role_id) REFERENCES public.roles("id ") NOT VALID;
+
+
+--
+-- TOC entry 3486 (class 2606 OID 32993)
+-- Name: orders shipping_address_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT shipping_address_fkey FOREIGN KEY (ship_address_id) REFERENCES public.addresses(id) NOT VALID;
+
+
+-- Completed on 2022-02-23 16:36:10 EST
+
+--
+-- PostgreSQL database dump complete
+--
+
+
+
 
 DROP DATABASE IF EXISTS flora_online_shop;
 CREATE DATABASE flora_online_shop;
